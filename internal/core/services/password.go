97c0a20090ca4c02a98be80830020d62b1564c95
CunhazadaNoDale <cunha12324@gmail.com>
2026-07-07 20:58:40 -0300

    feat: codigo de apoio para criptografar senha e checar

--- internal/core/services/password.go
+++ internal/core/services/password.go
@@ -1,13 +1,20 @@
 package services
 
+import "golang.org/x/crypto/bcrypt"
 
-func hashPassword(password string) string {
+func HashPassword(password string) string {
 	// Implement password hashing logic here (e.g., using bcrypt)
 	passwordHash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if err != nil {
-		// Handle error appropriately
-		return ""
+		return string(err.Error())
 	}
 	// For demonstration purposes, we'll just return the password as-is.
 	return string(passwordHash)
+}
+
+
+func CheckPasswordHash(password, hash string) bool {
+ 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	
+	return err == nil
 }
